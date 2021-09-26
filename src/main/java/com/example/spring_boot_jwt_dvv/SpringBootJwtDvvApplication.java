dba18ba939f4a083bb48c5e428dd65c476ad6a66
VanVinh983 <doanvinh983@gmail.com>
2021-09-26 11:11:44 +0700

    commit master branch

---
+++ src/main/java/com/example/spring_boot_jwt_dvv/SpringBootJwtDvvApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_dvv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtDvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtDvvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_jwt_dvv/SpringBootJwtDvvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_dvv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtDvvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
